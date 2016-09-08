cedfa668ae9d0796ceb776320fa31575f8fa20ab
liucol <1545766219@qq.com>
2016-09-08 13:26:58 +0800

    Signed-off-by: liucol <1545766219@qq.com>

---
+++ js/tanchupage.js
@@ -0,0 +1,10 @@
+/*function openwindow(){
+	window.open('tankuang.html','_self',
+		          'width:200px,heigth:200px');
+}*/
+function openwindow(){
+	var y;
+	var x;
+	y=prompt("请输入图书证号：");
+	x=prompt("请输入今日日期：");
+}
